2f3411d92b672f302aae2bce81bc1173ae2b29b4
layla-focalors <starlights2283@gmail.com>
2024-03-07 12:22:03 +0900

    add re deploy

--- haagentus/setup.py
+++ haagentus/setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="haagentus",
-    version="0.0.1",
+    version="0.0.9",
     author="Layla-focalors",
     author_email="sjhhjs2004@kakao.com",
     description="An statiscian package for python & very easy version",
-    install_requires=['math','numpy','scipy', 'statistics', 'pandas', 'matplotlib', 'stemgraphic'],
+    install_requires=['numpy','scipy', 'statistics', 'pandas', 'matplotlib', 'stemgraphic'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/layla-focalors/r2python",
@@ -21,7 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
